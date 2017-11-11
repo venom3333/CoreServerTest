73b3c811b5057334c60c0a345824f710d7024a33
Vladimir Venom <venom3333@gmail.com>
2017-11-11 13:35:17 +0300

    Добавьте файлы проекта.

---
+++ CoreServerTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CoreServerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Start the server  
+            TcpHelper.StartServer(5678);
+            TcpHelper.Listen(); // Start listening.  
+        }
+    }
+}
