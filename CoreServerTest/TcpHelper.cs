6ae9fa487b2e4e99d0fceb527efab8b8c7b7d416
Vladimir Venom <venom3333@gmail.com>
2017-11-11 17:48:15 +0300

    Параллельные мульти подключения к серверу.

--- CoreServerTest/TcpHelper.cs
+++ CoreServerTest/TcpHelper.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading.Tasks;
 
 namespace CoreServerTest
 {
@@ -24,33 +25,48 @@ namespace CoreServerTest
         public static void Listen()
         {
             if (Listener == null || !Accept) return;
-            
+
             // Continue listening.  
             while (true)
             {
                 Console.WriteLine("Waiting for client...");
-                var clientTask = Listener.AcceptTcpClientAsync(); // Get the client  
 
-                if (clientTask.Result == null) continue;
-                
-                Console.WriteLine("Client connected. Waiting for data.");
-                var client = clientTask.Result;
-                var message = "";
-
-                while (!message.StartsWith("quit"))
+                using (var clientTask = Listener.AcceptTcpClientAsync())
                 {
-                    var data = Encoding.ASCII.GetBytes("Send next data: [enter 'quit' to terminate] ");
-                    client.GetStream().Write(data, 0, data.Length);
 
-                    var buffer = new byte[1024];
-                    client.GetStream().Read(buffer, 0, buffer.Length);
+                    // var clientTask = Listener.AcceptTcpClientAsync(); // Get the client  
 
-                    message = Encoding.ASCII.GetString(buffer);
-                    Console.WriteLine(message.TrimEnd('\0').TrimEnd('\n'));
+                    if (clientTask.Result == null) continue;
+
+                    new Task(delegate { WorkWithClient(clientTask.Result); }).Start();
                 }
-                Console.WriteLine("Closing connection.");
-                client.GetStream().Dispose();
             }
+        }
+
+        public static void WorkWithClient(TcpClient client)
+        {
+            //var client = clientTask.Result;
+            Console.WriteLine($"New client connected ({DateTime.Now.ToString()}). Waiting for data...");
+            var message = "";
+
+            var data = Encoding.ASCII.GetBytes($"Connected to server: {Listener.LocalEndpoint.AddressFamily.ToString()}\n");
+            client.GetStream().Write(data, 0, data.Length);
+
+            while (!message.StartsWith("quit"))
+            {
+                data = Encoding.ASCII.GetBytes("Send next data ('quit' to terminate connection): ");
+                client.GetStream().Write(data, 0, data.Length);
+
+                var buffer = new byte[client.ReceiveBufferSize];
+                client.GetStream().Read(buffer, 0, client.ReceiveBufferSize);
+
+                message = Encoding.ASCII.GetString(buffer);
+                message = message.TrimEnd('\0').TrimEnd('\n');
+                Console.WriteLine(message);
+            }
+            Console.WriteLine("Closing connection.");
+            client.GetStream().Dispose();
+            client.Dispose();
         }
     }
 }
